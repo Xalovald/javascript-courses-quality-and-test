4afc7f1ec7ec8a1d86d28ed00b251a7536a7f660
Xalovald <92373762+Xalovald@users.noreply.github.com>
2024-12-17 09:56:27 +0100

    Update database.test.js

--- test/jest/database.test.js
+++ test/jest/database.test.js
@@ -27,11 +27,8 @@ describe('Database Functions', () => {
         });
     });
 
-    beforeEach(() => {
+    beforeEach((done) => {
         customDb = new CustomDatabase(false).initialize('./tests.db');
-    });
-
-    afterEach((done) => {
         customDb.db.serialize(() => {
             customDb.db.run(`
                 CREATE TABLE IF NOT EXISTS players (
@@ -41,15 +38,17 @@ describe('Database Functions', () => {
                     game_date DATE NOT NULL
                 )
             `);
-        });
-        customDb.db.serialize(() => {
             customDb.db.run(`
                 CREATE TABLE IF NOT EXISTS game_state (
                     id INTEGER PRIMARY KEY,
                     score INTEGER
                 )
             `);
+            done();
         });
+    });
+
+    afterEach((done) => {
         customDb.db.run('DELETE FROM players', (err) => {
             if (err) done(err);
             else done(); // Resolve done after cleaning up
@@ -57,14 +56,14 @@ describe('Database Functions', () => {
     });
 
     afterAll((done) => {
-        customDb.updatescore(1000, 'win')
+        customDb.updatescore(1000, 'win');
         customDb.db.close((err) => {
             if (err) done(err);
             else done();
         });
         jest.clearAllTimers(); // Clear timers to avoid memory leaks
     });
-
+    
     jest.setTimeout(20000); // Increase timeout to 20 seconds if needed
 
     test('should initialize database', () => {
